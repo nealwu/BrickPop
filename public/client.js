41d3e5b5614a531e3456e77155e060c9c8bb2aba
Neal Wu <neal@nealwu.com>
2016-12-26 21:39:27 -0600

    Random colors are now displayed

--- public/client.js
+++ public/client.js
@@ -1,18 +1,43 @@
 var GRID_SIZE = 10;
 var CELL_WIDTH = 80;
-var CIRCLE_FRACTION = 0.75;
+var CIRCLE_FRACTION = 0.8;
+
+var COLORS = ['red', 'yellow', 'green', 'blue', 'purple', 'saddlebrown'];
+var EMPTY = '.';
 
 var canvas = document.querySelector('canvas');
 canvas.width = GRID_SIZE * CELL_WIDTH;
 canvas.height = GRID_SIZE * CELL_WIDTH;
 var context = canvas.getContext('2d');
 
-for (var i = 0; i < GRID_SIZE; i++) {
-  for (var j = 0; j < GRID_SIZE; j++) {
-    context.beginPath();
-    context.arc(CELL_WIDTH * (i + 0.5), CELL_WIDTH * (j + 0.5), CELL_WIDTH * 0.5 * CIRCLE_FRACTION, 0, 2 * Math.PI, false);
-    context.fillStyle = ['red', 'yellow', 'green', 'blue', 'purple', 'gray'][Math.floor(Math.random() * 6)];
-    context.fill();
-    context.closePath();
+function updateDisplay(grid) {
+  for (var r = 1; r <= GRID_SIZE; r++) {
+    for (var c = 1; c <= GRID_SIZE; c++) {
+      context.beginPath();
+      context.arc(CELL_WIDTH * (r - 0.5), CELL_WIDTH * (c - 0.5), CELL_WIDTH * 0.5 * CIRCLE_FRACTION, 0, 2 * Math.PI, false);
+      context.fillStyle = grid[r][c] === EMPTY ? 'white' : COLORS[grid[r][c] - '0'];
+      context.fill();
+      context.closePath();
+    }
   }
 }
+
+var grid = [];
+
+for (var r = 0; r <= GRID_SIZE + 1; r++) {
+  var row = [];
+
+  for (var c = 0; c <= GRID_SIZE + 1; c++) {
+    row.push(EMPTY);
+  }
+
+  grid.push(row);
+}
+
+window.setInterval(function() {
+  var r = Math.floor(Math.random() * GRID_SIZE + 1);
+  var c = Math.floor(Math.random() * GRID_SIZE + 1);
+  var color = Math.floor(Math.random() * COLORS.length);
+  grid[r][c] = String.fromCharCode(color + '0'.charCodeAt(0));
+  updateDisplay(grid);
+}, 100);
