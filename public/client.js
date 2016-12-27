4f9e1195ce7379c539750228ab00f0b6e3752427
Neal Wu <neal@nealwu.com>
2016-12-26 21:49:02 -0600

    Implemented slideDown and fixed the x and y coordinates, which were swapped

--- public/client.js
+++ public/client.js
@@ -14,10 +14,35 @@ function updateDisplay(grid) {
   for (var r = 1; r <= GRID_SIZE; r++) {
     for (var c = 1; c <= GRID_SIZE; c++) {
       context.beginPath();
-      context.arc(CELL_WIDTH * (r - 0.5), CELL_WIDTH * (c - 0.5), CELL_WIDTH * 0.5 * CIRCLE_FRACTION, 0, 2 * Math.PI, false);
+      context.arc(CELL_WIDTH * (c - 0.5), CELL_WIDTH * (r - 0.5), CELL_WIDTH * 0.5 * CIRCLE_FRACTION, 0, 2 * Math.PI, false);
       context.fillStyle = grid[r][c] === EMPTY ? 'white' : COLORS[grid[r][c] - '0'];
       context.fill();
       context.closePath();
+    }
+  }
+}
+
+function slideDown(grid) {
+  var fill_row;
+  var fill_col = 1;
+
+  for (var c = 1; c <= GRID_SIZE; c++) {
+    fill_row = GRID_SIZE;
+
+    for (var r = GRID_SIZE; r >= 1; r--) {
+      if (grid[r][c] != EMPTY) {
+        grid[fill_row][fill_col] = grid[r][c];
+
+        if (r != fill_row || c != fill_col) {
+          grid[r][c] = EMPTY;
+        }
+
+        fill_row--;
+      }
+    }
+
+    if (fill_row < GRID_SIZE) {
+      fill_col++;
     }
   }
 }
@@ -37,7 +62,14 @@ for (var r = 0; r <= GRID_SIZE + 1; r++) {
 window.setInterval(function() {
   var r = Math.floor(Math.random() * GRID_SIZE + 1);
   var c = Math.floor(Math.random() * GRID_SIZE + 1);
-  var color = Math.floor(Math.random() * COLORS.length);
-  grid[r][c] = String.fromCharCode(color + '0'.charCodeAt(0));
+  var color = Math.floor(Math.random() * (COLORS.length + 1));
+
+  if (color < COLORS.length) {
+    grid[r][c] = String.fromCharCode(color + '0'.charCodeAt(0));
+  } else {
+    grid[r][c] = EMPTY;
+  }
+
+  slideDown(grid);
   updateDisplay(grid);
-}, 100);
+}, 50);
